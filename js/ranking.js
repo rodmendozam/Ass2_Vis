ce9c3a20ec3103eb88dc9f768eae4df464f08e74
Rodrigo Mendoza <rmendoza@mendozablanco.com.mx>
2016-01-26 11:50:59 +0100

    Dropdown menu fixed

--- js/ranking.js
+++ js/ranking.js
@@ -2,7 +2,26 @@
  * Created by s150834 on 25-1-2016.
  */
 
-
+//Variables
 var selected_year_from_list = 1991;
 
-console.log("The selected year is:" + selected_year_from_list);
+
+
+
+
+$(document).ready(function() {//when the DOM has loaded
+
+    $('#yearMenu li').click(function() {//add event listener to dropdown menu
+        $('#yearMenu li').removeClass('yearSelected');
+        $(this).addClass('yearSelected');
+        selected_year_from_list = $('.yearSelected').text();
+        $('#yearButton').text(selected_year_from_list);
+        //Update Map
+        year = selected_year_from_list;
+        updateMap(selected_year_from_list);
+        //UpdateRanking
+
+        //UpdateIndexChart
+    });
+
+});
