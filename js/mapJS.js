4bff274375e9d8848cd5192203be7cb3292159da
Hugo Spee <hugo.spee@upcmail.nl>
2016-01-26 11:51:01 +0100

    ranges

--- js/mapJS.js
+++ js/mapJS.js
@@ -54,7 +54,7 @@ function initMap(year, myData){
                     output = parseFloat(parseFloat(myData[year-1990][geo.id]).toFixed(4)) + "%";
                     for (i = year-1; i >= 1990; i--){
                         if (myData[i-1990][geo.id] != ""){
-                            console.log(original);
+                            //console.log(original);
                             change = parseFloat(parseFloat((original / parseFloat(myData[i-1990][geo.id]).toFixed(4))).toFixed(4));
                             //console.log(myData[i-1990][geo.id]);
                             output2 = "change from " + i + ": " + change;
@@ -85,33 +85,41 @@ function updateMap(year){
         return console.log("error year not within bounds");
     }
     year = year - 1990;
-    var max = 0
+    var max = 0;
     for (var key in myData[year]) {
+
         if (myData[year].hasOwnProperty(key)) {
-            if (myData[year][key] > max){
+            if (parseFloat(myData[year][key]) > parseFloat(max)){
                 max = myData[year][key];
             }
         }
     }
+    max = parseFloat(parseFloat(max).toFixed(4));
     var range = max / 5.0;
+    range = parseFloat(parseFloat(range).toFixed(4));
 
 
 
     for (key in myData[year]) {
-
+        console.log(key);
         if (myData[year].hasOwnProperty(key)) {
             var data = {};
             var value = myData[year][key];
             if (value > 0 && value < range) {
                 data[key] = 'rgba(253,208,162,0.9)';
+                $('#class1').text(parseFloat(0.001) + "-" + parseFloat(parseFloat(range-0.001).toFixed(4)) + "%");
             } else if (value >= range && value < 2*range){
                 data[key] = 'rgba(253,174,107,0.9)';
+                $('#class2').text(parseFloat(parseFloat(range).toFixed(4)) + "-" + parseFloat(parseFloat(2*range-0.001).toFixed(4)) + "%");
             } else if (value >= 2*range && value < 3*range){
                 data[key] = 'rgba(253,141,60,0.9)';
+                $('#class3').text(parseFloat(parseFloat(2*range).toFixed(4)) + "-" + parseFloat(parseFloat(3*range-0.001).toFixed(4)) + "%");
             } else if (value >= 3*range && value < 4*range){
                 data[key] = 'rgba(230,85,13,0.9)';
+                $('#class4').text(parseFloat(parseFloat(3*range).toFixed(4)) + "-" + parseFloat(parseFloat(4*range-0.001).toFixed(4)) + "%");
             } else if (value >= 4*range && value <= max){
                 data[key] = 'rgba(166,54,3,0.9)';
+                $('#class5').text(">" + parseFloat(parseFloat(4*range).toFixed(4)) + "%");
             } else {
                 data[key] = "grey";
             }
